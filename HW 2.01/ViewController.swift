2fe71d5e98c952c9b3fc14181526ce0ef3418fb9
feeeeeen4ik <feeeeen4ik1994@gmail.com>
2024-10-25 16:13:06 +0500

    Initial Commit

---
+++ HW 2.01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 2.01
+//
+//  Created by Феликс Антонович on 25.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
